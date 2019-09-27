42eb2352cae648cac8630b2497a7465ed28cb6e7
Stijn Polfliet <spolfliet@newrelic.com>
2019-09-27 20:08:29 +0200

    Install snmp

--- snmp/init.sh
+++ snmp/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install snmpsim
-snmpsimd --data-dir=./assets --agent-udpv4-endpoint=127.0.0.1:1024
+apt-get install snmp snmpsim
+
+snmpsimd --data-dir=./assets --agent-udpv4-endpoint=127.0.0.1:1024 &
