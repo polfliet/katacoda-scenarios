c282c1129865b268858174f47f454ddf0e7fe437
Stijn Polfliet <spolfliet@newrelic.com>
2019-09-27 20:32:04 +0200

    Set permissions

--- snmp/init.sh
+++ snmp/init.sh
@@ -1,5 +1,8 @@
 echo "Installing snmp and snmpsim..."
 apt install snmp snmpsim -y
 
+echo "Setting permissions"
+chmod 777 -R /root/
+
 echo "Running snmpsimd in background..."
-snmpsimd --process-user=nobody --process-group=nogroup --data-dir=/root/ --agent-udpv4-endpoint=127.0.0.1:1024 &
+snmpsimd --process-user=nobody --process-group=nogroup --data-dir=/root/ --agent-udpv4-endpoint=127.0.0.1:1025 --daemonize &
