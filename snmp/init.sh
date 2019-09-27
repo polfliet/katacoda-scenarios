0d01a1a6624e51ce6ed899c654944964dd0c59fa
Stijn Polfliet <spolfliet@newrelic.com>
2019-09-27 20:24:42 +0200

    Update port and data-dir

--- snmp/init.sh
+++ snmp/init.sh
@@ -2,4 +2,4 @@ echo "Installing snmp and snmpsim..."
 apt install snmp snmpsim -y
 
 echo "Running snmpsimd in background..."
-snmpsimd --process-user=nobody --process-group=nogroup --data-dir=./assets --agent-udpv4-endpoint=127.0.0.1:61024 &
+snmpsimd --process-user=nobody --process-group=nogroup --data-dir=/root/ --agent-udpv4-endpoint=127.0.0.1:1024 &
