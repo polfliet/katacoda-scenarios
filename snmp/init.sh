e4dabfff6e5c677082e73d96af8ad4e18a34ca30
Stijn Polfliet <spolfliet@newrelic.com>
2019-09-27 20:17:44 +0200

    Print output

--- snmp/init.sh
+++ snmp/init.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+echo "Installing snmp and snmpsim..."
+apt install snmp snmpsim
 
-apt-get install snmp snmpsim
-
+echo "Running snmpsimd in background..."
 snmpsimd --data-dir=./assets --agent-udpv4-endpoint=127.0.0.1:1024 &
